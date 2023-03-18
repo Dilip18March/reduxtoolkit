6006fbcb4ee65cb3a966a89d4f17d202807d7c07
dilip-kumar-mit <dilip.kumar.bca.2019@mitmeerut.ac.in>
2023-03-18 11:45:13 +0530

    reduxtoolkit

---
+++ reduxtoolkit/src/components/Products.js
@@ -0,0 +1,98 @@
+import React, {  useEffect } from 'react'
+import { useDispatch , useSelector} from 'react-redux';
+import { add } from '../store/cartSlice';
+
+import {STATUS} from '../store/productSlice'
+
+import { fetchProducts } from '../store/productSlice';
+
+const Products = () => {
+
+	const dispatch = useDispatch();
+
+	const { data:products,status}=useSelector((state)=>state.product)
+
+
+	//const [products, setProducts] = useState([])
+	
+	useEffect(() => {
+
+		dispatch(fetchProducts());
+		
+
+	//	const fetchProducts = async () => {
+
+			//const res = await fetch('https://fakestoreapi.com/products');
+
+			//const data = await res.json();
+			
+		//	console.log(data);
+
+			//setProducts(data);
+
+
+
+			
+		//}
+
+		//fetchProducts();
+
+		
+	}, [])
+	
+	const handleAdd = (product) => {
+
+	//product
+
+		dispatch(add(product));
+		
+		
+		
+		
+
+	}
+
+	if(status===STATUS.LOADING) {
+
+		return <h2>LOADING.......</h2>
+
+
+
+	}
+
+		return (
+			<div className='productsWrapper'>
+				
+				{products.map((product) => (
+
+					<div className='card' key={product.id}>
+						
+						<img src={product.image} alt='' />
+						
+						<h4>{product.title}</h4>
+
+						<h5>{product.price}</h5>
+
+						<button onClick={()=>handleAdd(product)} className='btn'>Add to cart</button>
+
+
+
+
+
+
+					</div>
+					
+				
+				
+				
+				
+				))}
+				
+
+		
+						
+				</div>
+		)
+}
+
+export default Products
